a088c2f3f94b2dcbfc08cdc4dd557ede5a598739
Julien TRITZ <julien.tritz@cgi.com>
2022-12-30 02:25:58 +0100

    delete publish directory

--- CITools/Program.cs
+++ CITools/Program.cs
@@ -22,7 +22,6 @@ namespace CITools
 
         static void Main(string[] args)
         {
-            Console.WriteLine("TEST UPDATE");
             var version = Assembly.GetExecutingAssembly().GetName().Version;
             string welcome = $"CITools {version}\n\nFirst time here, use first \'config -input {{inputfilepath}} -output {{outputfolderpath}}\'\n";
             Console.WriteLine(welcome);
